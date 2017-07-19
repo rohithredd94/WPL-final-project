f4c3339b317702a06dbabcabebde6d535bff02ea
Silpy Jain <Silpy Jain>
2017-07-19 18:17:34 -0500

    changes for login page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,1 +1,11 @@
 var app = angular.module('MoviesRating', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'partials/login.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
