8198fa63b240c666bcb33b6394454f98f75e63a0
Rohith Reddy <rohithreddyk94@gmail.com>
2017-07-19 18:34:30 -0500

    Java Parser Code

--- movie_parser.java
+++ movie_parser.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
 import org.json.*;
 import com.mongodb.*;
 //import org.json.simple.parser.JSONParser;
@@ -31,22 +33,60 @@ public class movie_parser{
       is.close();
     }
   }
+    
+    public static JSONArray concatArray(JSONArray arr1, JSONArray arr2)
+            throws JSONException {
+        JSONArray result = new JSONArray();
+        for (int i = 0; i < arr1.length(); i++) {
+            result.put(arr1.get(i));
+        }
+        for (int i = 0; i < arr2.length(); i++) {
+            result.put(arr2.get(i));
+        }
+        return result;
+    }
 
     public static void main(String[] args) throws IOException, JSONException {
-        JSONObject obj = readJsonFromUrl("https://api.themoviedb.org/3/movie/now_playing?api_key=d5cb409394cf931c2fea8fef9c3aa05f&page=1");
-        System.out.println(obj.toString());
-        //JSONParser parser = new JSONParser();
-        JSONArray jsonarray = (JSONArray)obj.get("results");
-        //JSONObject obj1 = (JSONObject)obj.get("results");
-        //System.out.println(obj1);
-        for (int i = 0; i < jsonarray.length(); i++) {
-            JSONObject jsonobject = jsonarray.getJSONObject(i);
+    	JSONArray jsonarray = new JSONArray();
+    	JSONArray json_cast = new JSONArray();
+    	JSONArray json_crew = new JSONArray();
+    	for(int i=1;i<=5;i++) {
+        JSONObject obj = readJsonFromUrl("https://api.themoviedb.org/3/discover/movie?api_key=d5cb409394cf931c2fea8fef9c3aa05f&year=2017&sort_by=popularity.desc&page="+i);
+        JSONArray jsonarray1 = (JSONArray)obj.get("results");
+        for(int j=0;j<jsonarray1.length();j++) {
+        	JSONObject temp = (JSONObject) jsonarray1.get(j);
+        	int movie_id=temp.getInt("id");
+        	JSONObject obj2 = readJsonFromUrl("https://api.themoviedb.org/3/movie/"+movie_id+"/credits?api_key=d5cb409394cf931c2fea8fef9c3aa05f");
+        	JSONObject cast = new JSONObject();
+        	cast.put("id", movie_id);
+        	JSONArray jsonarray2 = (JSONArray)obj2.get("cast");
+        	cast.append("cast", jsonarray2);
+        	json_cast.put(cast);
+        	try        
+        	{
+        	    Thread.sleep(200);
+        	} 
+        	catch(InterruptedException ex) 
+        	{
+        	    Thread.currentThread().interrupt();
+        	}
+        }
+        
+        jsonarray = concatArray(jsonarray, jsonarray1);
+        /*for (int j = 0; i < jsonarray.length(); i++) {
+            JSONObject jsonobject = jsonarray.getJSONObject(j);
             String name = jsonobject.getString("overview");
             //String url = jsonobject.getString("url");
             //System.out.println(name);
-        }
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        mongoClient.getDatabaseNames().forEach(System.out::println);
+        }*/
         
-    }
+    	}
+    	System.out.println(json_cast);
+    	/*MongoClient mongoClient = new MongoClient("localhost", 27017);
+        mongoClient.getDatabaseNames().forEach(System.out::println);
+        DB db = mongoClient.getDB("vidzy");
+        Set<String> names = db.getCollectionNames();*/
+        //System.out.println(names);
+        //System.out.println(jsonarray);
+    	}
 }
