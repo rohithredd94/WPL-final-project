80952f6ea44c913f80a74e3753840789f7d003dc
Tharunn Golthi <txg160430@utdallas.edu>
2017-07-19 17:45:17 -0500

    genres api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var movies = require('./routes/movies');
+var genres = require('./routes/genres');
 
 var app = express();
 
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api/movies', movies);
-
+app.use('/api/genres', genres);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
