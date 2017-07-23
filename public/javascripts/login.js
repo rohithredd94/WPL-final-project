0d401083fb90af4fb81250e2cf3435374ac80f48
Tharunn Golthi <txg160430@utdallas.edu>
2017-07-23 02:38:59 -0500

    Home, login and sign-up pages
    -Added home login and sign-up pages

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,1 +1,21 @@
 var app = angular.module('MoviesRating', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            //controller: 'HomeCtrl'
+        })
+        .when('/signup', {
+            templateUrl: 'partials/signup.html',
+            //controller: 'HomeCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'partials/login.html',
+            //controller: 'HomeCtrl'
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
